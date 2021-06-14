064fe57712f945b2492a8a0afd53787df96d1a81
SaltyBr <SaltyBrain@yandex.ru>
2021-06-14 16:47:11 +0300

    onChanges

--- angular-tour-of-heroes/src/app/child/child.component.ts
+++ angular-tour-of-heroes/src/app/child/child.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges {
   
   @Input() item = ''; // decorate the property with @Input()
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges Child Component');
+    
+  }
 
   ngOnInit(): void {
   }
